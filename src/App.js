bce20ca6d6d58b337747e001754d5e6f637916ae
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-06-19 23:03:55 +0530

    create signuplogin page add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/signup',
+    element: <SignupPage />
+  },
+]);
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }

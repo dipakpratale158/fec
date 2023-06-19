5ec0b1aad7b484b8bfacc0f4930c480f02ee72cc
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-06-20 03:54:08 +0530

    fetch product using redux dummy data json server

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -1,9 +1,9 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, incrementAsync, selectCount } from '../productSlice';
+import { fetchAllProductsAsync, selectAllProducts } from '../productSlice';
 import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
+import { ChevronLeftIcon, ChevronRightIcon , StarIcon} from '@heroicons/react/20/solid'
 
 import {
   ChevronDownIcon,
@@ -13,6 +13,7 @@ import {
   Squares2X2Icon,
 } from '@heroicons/react/20/solid';
 import { Link } from 'react-router-dom';
+import { fetchAllProducts } from '../productAPI';
 
 const sortOptions = [
   { name: 'Most Popular', href: '#', current: true },
@@ -64,14 +65,14 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-const products = [
+const oldproducts = [
   {
     id: 1,
     name: 'Basic Tee',
     href: '#',
-    imageSrc:
+    thumbnail:
       'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
+    title: "Front of men's Basic Tee in black.",
     price: '$35',
     color: 'Black',
   },
@@ -79,9 +80,9 @@ const products = [
     id: 2,
     name: 'Basic Tee',
     href: '#',
-    imageSrc:
+    thumbnail:
       'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
+    title: "Front of men's Basic Tee in black.",
     price: '$35',
     color: 'Black',
   },
@@ -89,18 +90,28 @@ const products = [
     id: 3,
     name: 'Basic Tee',
     href: '#',
-    imageSrc:
+    thumbnail:
       'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
+    title: "Front of men's Basic Tee in black.",
     price: '$35',
     color: 'Black',
   },
 ];
 
+
+
 export default function ProductList() {
-  const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+const products=useSelector(selectAllProducts)
+
+
+useEffect(()=>{
+  dispatch(fetchAllProductsAsync())
+},[dispatch])
+
+
+
 
   return (
     <div className="bg-white">
@@ -360,32 +371,37 @@ export default function ProductList() {
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                       {products.map((product) => (
                         <Link to='/product-detail'>
-                        <div key={product.id} className="group relative">
-                          <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                        <div key={product.id} className="group relative border-solid border-2 p-2 border-gray-200">
+                          <div className="min-h-60 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60">
                             <img
-                              src={product.imageSrc}
-                              alt={product.imageAlt}
+                              src={product.thumbnail}
+                              alt={product.title}
                               className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                             />
                           </div>
                           <div className="mt-4 flex justify-between">
                             <div>
                               <h3 className="text-sm text-gray-700">
-                                <a href={product.href}>
+                                <a href={product.thumbnail}>
                                   <span
                                     aria-hidden="true"
                                     className="absolute inset-0"
                                   />
-                                  {product.name}
+                                  {product.title}
                                 </a>
                               </h3>
-                              <p className="mt-1 text-sm text-gray-500">
-                                {product.color}
+                              <p className="mt-1 text-sm text-yellow-500">
+                               <StarIcon className='w-6 h-6 inline'></StarIcon>
+                               <span className='align-bottom'> {product.rating}</span> 
                               </p>
                             </div>
                             <p className="text-sm font-medium text-gray-900">
                               {product.price}
                             </p>
+                            <p className="text-sm font-medium text-gray-900">
+
+                            ${Math.round(product.price *(1 - product.discountPercentage / 100))}
+</p>
                           </div>
                         </div>
                         </Link>
